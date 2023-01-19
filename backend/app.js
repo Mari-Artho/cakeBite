a33856408078da90fde0f96c6a9fd87c5909869c
Mari-Artho <mari.artho@medieinstitutet.se>
2023-01-19 19:59:44 +0100

    Create cakes router.

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var adminRouter = require('./routes/admin');
 var cafeRouter = require('./routes/cafe');
+var cakesRouter = require('./routes/cakes');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin', adminRouter);
 app.use('/cafe', cafeRouter);
+app.use('/cakes', cakesRouter);
 
 app.locals.con = mysql.createConnection({
     host: "localhost",
