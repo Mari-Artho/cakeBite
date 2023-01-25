aec7cb958f82c80ad0425840dc9380c65c9ddb95
Mari-Artho <mari.artho@medieinstitutet.se>
2023-01-25 17:17:44 +0100

    Change back button layout.

--- src/views/CafeView.vue
+++ src/views/CafeView.vue
@@ -17,9 +17,14 @@ getCafe();
 
 <template>
     <div class="cafe">
-        <router-link v-bind:to="{name:'home'}">
-            BACK
-        </router-link>
+     
+            <h3>
+                <router-link v-bind:to="{name:'home'}">
+                BACK 
+                </router-link> > Cafe 
+            </h3>
+      
+        
       <h1>Hello this is a CafeView.vue</h1>
       <p>id : {{$route.params.id}}</p>
       <p>name : {{$route.params.name}}</p>
@@ -31,3 +36,15 @@ getCafe();
       // TODO: query backend /cafe/:id to get data on cafe
     </div>
 </template>
+
+<style scoped lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Aref+Ruqaa+Ink:wght@700&display=swap');
+@import '../assets/styles.scss';
+@media (min-width: 1024px) {
+.cafe {
+    min-height: 100vh;
+    // background-color: whitesmoke;
+    // border-radius: 50% 10% 10% 40%;
+}
+}
+</style>
