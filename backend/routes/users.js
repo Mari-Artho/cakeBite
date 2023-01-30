004c25b01284c309cf0bdbd3fd6972380b19596c
Mari-Artho <mari.artho@medieinstitutet.se>
2023-01-30 10:25:17 +0100

    Delete TestView.vue

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,16 +1,16 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res) {
-  res.setHeader('content-type', 'application/json; charset=utf-8');
-  req.app.locals.con.connect(function(err){
-    if(err){
-      console.log(err);
-    }
-  });
-});
+// /* GET users listing. */
+// router.get('/', function(req, res) {
+//   res.setHeader('content-type', 'application/json; charset=utf-8');
+//   req.app.locals.con.connect(function(err){
+//     if(err){
+//       console.log(err);
+//     }
+//   });
+// });
 
-console.log("Now running user.js");
+// console.log("Now running user.js");
 
-module.exports = router;
+// module.exports = router;
--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
-//import Login from './LoginView.vue'
 
 //get data from mySql
 const admins = ref([]);
