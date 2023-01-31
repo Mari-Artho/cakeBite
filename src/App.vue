0e33439f95c60b147de2aeafdf33a81fa4c637d8
Mari-Artho <mari.artho@medieinstitutet.se>
2023-01-31 16:19:13 +0100

    Create Footer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 </script>
 
 <template>
@@ -16,6 +17,7 @@ import Header from './components/Header.vue'
     </div>
 
     <RouterView />
+    <Footer msg="hello"/>
   </section>
 </template>
 
---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+    <h1>This is footer</h1>
+  {{ msg }}
+</template>
+
+<style scoped lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Aref+Ruqaa+Ink:wght@700&display=swap');
+@import '../assets/styles.scss';
+
+h1 {
+  font-size: 2rem;
+}
+
+</style>
