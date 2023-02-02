2986064200eb5c2eef3549738b7972b643a87b32
Mari-Artho <mari.artho@medieinstitutet.se>
2023-02-02 12:07:27 +0100

    Change citat css in About us

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -12,11 +12,12 @@ function joinBtn(){
         One day, Maria, the founder of the CakeBite went to her favorite cake shop in Stockholm.
         She wanted to eat strawberry shortcake there. Unfortunately, the cake was sold out.
         <br/>
-        The next time, this time the matcha milk crepe was sold out. At the time, She thought, "I want to know the inventory of the cakes before going to the store.
-        Wouldn't it be nice if people could do that?”
+        The next time, this time the matcha milk crepe was sold out. At the time, She thought, "I wish I could check the inventory before going to the cake shop.
+        Wouldn't it be nice if people could do that?"
         This was the moment CakeBite was born.
         <blockquote>
-          <i> "Where there is a will, there is a way", Abraham Lincoln</i>
+          <i> Where there is a will, there is a way.  <br/>Abraham Lincoln </i>
+         
         </blockquote>
       </article>
       
@@ -28,6 +29,10 @@ function joinBtn(){
 
     <h1>Customer's voice</h1>
 
+    <h1>How to use the CafeBite</h1>
+
+    <h1>Our business</h1>
+
 
     <button class="joinBtn" v-on:click="joinBtn()">Click here to join us!</button>
   </section>
@@ -37,42 +42,60 @@ function joinBtn(){
 @import url('https://fonts.googleapis.com/css2?family=Aref+Ruqaa+Ink:wght@700&display=swap');
 @import '../assets/styles.scss';
 @media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    color: $text-grey;
-  }
+.about {
+  min-height: 100vh;
+  color: $text-grey;
+}
 
-  article {
-    //width: 700px;
-    //margin: auto;
-    font-size: 1.4rem;
-    text-align: left;
-    padding:  1rem;
-    //background-color: white;
-  }
-
-  .ourStory {
-    display: flex;
-  }
-
-  .ourStory-img {
-    width: 30rem;
-    height: 22rem;
-    border-radius: 1rem;
-    margin-top: 2rem;
-  }
-
-  .joinBtn {
-  background-color: #CC4777;
-  width: 30rem;
-  height: 4rem;
+h1 {
   font-size: 2rem;
-  font-family: $main-font;
-  cursor: pointer;
-  &:hover{
-    background-color: $main-pink;
-    color: white;
-  }
+  font-family: $sub-font;
+}
+
+article {
+  font-size: 1.4rem;
+  text-align: left;
+  padding:  1rem;
+}
+
+blockquote::before {
+  content: '\201C';
+  font-size: 1.5rem;
+  top: 1.5rem;
+  left: 0;
+}
+
+blockquote::after {
+  content: '\201D';
+  font-size: 1.5rem;
+}
+
+.ourStory {
+  display: flex;
+}
+
+.ourStory-img {
+  width: 30rem;
+  height: 22rem;
+  border-radius: 1rem;
+  margin-top: 2rem;
+}
+
+.joinBtn {
+background-color: $main-pink;
+color: white;
+width: 20rem;
+height: 4rem;
+font-size: 2rem;
+font-family: $main-font;
+margin: 2rem;
+border-radius: 1rem;
+cursor: pointer;
+&:hover{
+  //background-color: rgb(246, 71, 100);
+  background: linear-gradient($main-pink, white);
+  color: white;
+}
 }
 }
 </style>
