9354e9b7a223b74db82fe1d119e5b17724c3f8f5
Mari-Artho <mari.artho@medieinstitutet.se>
2023-06-30 19:41:32 +0200

    Make port configurable.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+const express = require('express')
+const serveStatic = require('serve-static')
 
 import App from './App.vue'
 import router from './router'
 
 import './assets/styles.scss'
 
-const app = createApp(App)
+const port = process.env.PORT || 8080
+
+const app = express() // && createApp(App)
+app.listen(port)
+console.log(`Front-end is listening on port: ${port}`)
 
 app.use(createPinia())
 app.use(router)
